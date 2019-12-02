b56b4f7eb7d3a397c69c3a7a3030366afc3165fe
pierre@pcmolinaro.name <pierre@pcmolinaro.name>
2019-12-02 17:52:04 +0100

    Suite

--- carte-esp32-minikit/test-carte-esp32-minikit/test-carte-esp32-minikit.ino
+++ carte-esp32-minikit/test-carte-esp32-minikit/test-carte-esp32-minikit.ino
@@ -4,64 +4,63 @@
 #endif
 
 // include the library code:
-//#include <LiquidCrystal.h>
+#include <LiquidCrystal.h>
 
-const byte L0 =  3 ;
-const byte L1 =  1 ;
-const byte L2 = 22 ;
-const byte L3 = 23 ;
-const byte L4 = 12 ;
+const byte L0 =  21 ;
+const byte L1 =  12 ; // TDI
 
 
 const byte P0 = 36 ;
-const byte P1 = 13 ;
-const byte P2 = 11 ;
-const byte P3 = 35 ;
-const byte P4 = 34 ;
+const byte ENCODER_CLIC = 39 ; // SVN
+const byte ENCODER_B = 35 ;
+const byte ENCODER_A = 34 ;
 
 // initialize the library by associating any needed LCD interface pin
 // with the arduino pin number it is connected to
-const byte rs = 9 ;
-const byte en = 8 ;
-const byte d0 = 0 ;
-const byte d1 = 1 ;
-const byte d2 = 2 ;
-const byte d3 = 3 ;
-const byte d4 = 4 ;
-const byte d5 = 5 ;
-const byte d6 = 6 ;
-const byte d7 = 7 ;
-//LiquidCrystal lcd (rs, en, d0, d1, d2, d3, d4, d5, d6, d7) ;
-//LiquidCrystal lcd (rs, en, d4, d5, d6, d7) ;
+const byte rs = 22 ;
+const byte en = 13 ; // TCK
+const byte d4 = 15 ; // TDO
+const byte d5 = 14 ; // TMS
+const byte d6 = 0 ;
+const byte d7 = 33 ;
+
+LiquidCrystal lcd (rs, en, d4, d5, d6, d7) ;
 
 void setup() {
 //--- Leds
+  pinMode (LED_BUILTIN, OUTPUT) ;
   pinMode (L0, OUTPUT) ;
   pinMode (L1, OUTPUT) ;
-  pinMode (L2, OUTPUT) ;
-  pinMode (L3, OUTPUT) ;
-  pinMode (L4, OUTPUT) ;
 //--- Poussoirs
-  pinMode (P0, INPUT_PULLUP) ;
-  pinMode (P1, INPUT_PULLUP) ;
-  pinMode (P2, INPUT_PULLUP) ;
-  pinMode (P3, INPUT_PULLUP) ;
-  pinMode (P4, INPUT_PULLUP) ;
-  // set up the LCD's number of columns and rows:
- // lcd.begin (20, 4);
+  pinMode (P0, INPUT) ;
+  pinMode (ENCODER_CLIC, INPUT) ;
+  pinMode (ENCODER_A, INPUT) ;
+  pinMode (ENCODER_B, INPUT) ;
+ // set up the LCD's number of columns and rows:
+  lcd.begin (20, 4) ;
   // Print a message to the LCD.
-//  lcd.print("hello, world!");
+  lcd.print ("hello, world!");
 }
 
+static uint32_t gLedBuiltinBlinkDate ;
+static uint32_t gLedL1BlinkDate ;
+static uint32_t gDisplayDate ;
+
 void loop() {
+  if (gLedBuiltinBlinkDate <= millis ()) {
+    gLedBuiltinBlinkDate += 1001 ;
+    digitalWrite (LED_BUILTIN, !digitalRead (LED_BUILTIN)) ;
+  }
+  if (gLedL1BlinkDate <= millis ()) {
+    gLedL1BlinkDate += 999 ;
+    digitalWrite (L1, !digitalRead (L1)) ;
+  }
   digitalWrite (L0, !digitalRead (P0)) ;
-  digitalWrite (L1, !digitalRead (P1)) ;
-  digitalWrite (L2, !digitalRead (P2)) ;
-  digitalWrite (L3, !digitalRead (P3)) ;
-  digitalWrite (L4, !digitalRead (P4)) ;
  // set the cursor to column 0, line 1
   // (note: line 1 is the second row, since counting begins with 0):
-//  lcd.setCursor (0, 1);
-  // print the number of seconds since reset:
-//  lcd.print (millis() / 1000);
+  if (gDisplayDate <= millis ()) {
+    gDisplayDate += 500 ;
+    lcd.setCursor (0, 1);
+    lcd.print (millis() / 1000);
+  }
 }
---
+++ carte-esp32-minikit/test-carte/test-carte.ino
@@ -0,0 +1,75 @@
+#include <LiquidCrystal.h>
+#include <TimerOne.h>
+
+LiquidCrystal lcd (18, 17, 16, 15, 14, 19); //liaison 4 bits de données
+
+void setup() {
+  pinMode (3, OUTPUT) ;
+  pinMode (4, OUTPUT) ;
+  pinMode (5, OUTPUT) ;
+  pinMode (6, OUTPUT) ;
+  pinMode (7, OUTPUT) ;
+  pinMode (13, OUTPUT) ;
+  pinMode (8, INPUT_PULLUP) ;
+  pinMode (9, INPUT_PULLUP) ;
+  pinMode (10, INPUT_PULLUP) ;
+  pinMode (11, INPUT_PULLUP) ;
+  pinMode (12, INPUT_PULLUP) ;
+  pinMode (2, INPUT_PULLUP) ; // ENCODER CLIC
+  pinMode (1, INPUT_PULLUP) ; // ENCODER B
+  pinMode (0, INPUT_PULLUP) ; // ENCODER A
+  lcd.begin(20,4); //utilisation d'un écran 20 colonnes et 4 lignes
+  lcd.write ("Programme de test"); //petit test pour vérifier que tout marche
+  analogWriteFrequency (23, 25000); // PWM Frequency 25 kHz
+  analogWriteResolution (6);  // analogWrite value 0 to 63
+  Timer1.initialize (1000) ; // 1000 micro s.
+  Timer1.attachInterrupt (encoderHandler);
+}
+
+volatile int gEncodeur = 0 ;
+int gEncodeurCache = 0 ;
+bool gEncoderA = false ;
+int gClignotement = 0 ;
+
+void encoderHandler (void) {
+  const bool encoderA = digitalRead (0) ;
+  if (encoderA && !gEncoderA) {
+    if ((gEncodeur < 63) && !digitalRead (1)) {
+      gEncodeur ++ ;
+    }else if ((gEncodeur > 0) && digitalRead (1)) {
+      gEncodeur -- ;
+    }
+  }
+  gEncoderA = encoderA ;
+  gClignotement ++ ;
+  if (gClignotement == 250) {
+    gClignotement = 0 ;
+    digitalWrite (13, !digitalRead (13)) ;
+  }
+}
+
+int gPWM = 0 ;
+
+void loop() {
+  delay (100) ;
+  digitalWrite (3, ! digitalRead (8)) ;
+  digitalWrite (4, ! digitalRead (9)) ;
+  digitalWrite (5, ! digitalRead (10)) ;
+  digitalWrite (6, ! digitalRead (11)) ;
+  digitalWrite (7, ! digitalRead (12)) ;
+  lcd.setCursor (0, 3) ;
+  if (!digitalRead (2)) {
+    lcd.print ("CLIC") ;
+  }else{
+    lcd.print ("    ") ;
+  }
+  if (gEncodeurCache != gEncodeur) {
+    gEncodeurCache = gEncodeur ;
+    lcd.setCursor (0, 1) ;
+    lcd.print ("  ") ;
+    lcd.setCursor (0, 1) ;
+    lcd.print (gEncodeurCache) ;
+  }
+  analogWrite (23, gPWM) ;
+  gPWM = (gPWM + 3) % 64 ;
+}
