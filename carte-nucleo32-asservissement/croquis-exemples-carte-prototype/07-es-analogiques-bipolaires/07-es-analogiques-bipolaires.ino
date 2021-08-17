6c5cd2757a062bd2df0f662e3e4337dd63387340
pierremolinaro <pierre@pcmolinaro.name>
2021-08-17 22:47:14 +0200

    Carte asservissement

---
+++ carte-nucleo32-asservissement/croquis-exemples-carte-prototype/07-es-analogiques-bipolaires/07-es-analogiques-bipolaires.ino
@@ -0,0 +1,84 @@
+
+#include "carte-asservissement.h"
+
+//--------------------------------------------------------
+
+void setup () {
+  configurerCarteAsservissement () ;
+  fixerGammeEncodeur (-128, 127) ;
+  lcd.print ("E/S analog. -10V:10V") ;
+  lcd.setCursor (0, 1) ;
+  lcd.print ("S0,S1:") ;
+  lcd.setCursor (0, 2) ;
+  lcd.print ("E0:") ;
+  lcd.setCursor (0, 3) ;
+  lcd.print ("E1:") ;
+}
+
+//--------------------------------------------------------
+
+static uint32_t gInstantClignotement = 0 ;
+static uint32_t gInstantLectureAnalogique = 0 ;
+static int32_t  gValeurEncodeur = -1 ;
+static int16_t gEntreeAnalogique0 = INT16_MAX ;
+static int16_t gEntreeAnalogique1 = INT16_MAX ;
+
+//--------------------------------------------------------
+
+void loop () {
+//--- Première action : clignotement led
+  if (gInstantClignotement <= millis ()) {
+  //--- Calcul de la prochaine échéance
+    gInstantClignotement += 1000 ;
+  //--- Faire clignoter LED_BUILTIN
+    digitalWrite (LED_BUILTIN, !digitalRead (LED_BUILTIN)) ;
+  }
+//--- Deuxième action : lire l'encodeur pour fixer les sorties analogiques
+  const int32_t encodeur = valeurEncodeur () ;
+  if (gValeurEncodeur != encodeur) {
+    gValeurEncodeur = encodeur ;
+    const int8_t commande = int8_t (gValeurEncodeur) ;
+    actionSortieAnalogiqueBipolaire (SORTIE_ANALOGIQUE_BIPOLAIRE::SB0, commande) ;
+    actionSortieAnalogiqueBipolaire (SORTIE_ANALOGIQUE_BIPOLAIRE::SB1, commande) ;
+    lcd.setCursor (6, 1) ;
+    lcd.print ("              ") ;
+    lcd.setCursor (6, 1) ;
+    lcd.print (commande) ;
+    lcd.print ("->") ;
+    lcd.print (float (commande) * 0.082824) ;
+    lcd.print ("V") ;
+  }
+//--- Troisième action : lire les entrées analogiques
+  if (gInstantLectureAnalogique <= millis ()) {
+    gInstantLectureAnalogique += 500 ;
+    const int16_t entree0 = lireEntreeAnalogiqueBipolaire (ENTREE_ANALOGIQUE_BIPOLAIRE::EB0) ;
+    if (gEntreeAnalogique0 != entree0) {
+      gEntreeAnalogique0 = entree0 ;
+      lcd.setCursor (4, 2) ;
+      lcd.print ("               ") ;
+      lcd.setCursor (4, 2) ;
+      lcd.print (entree0) ;
+      lcd.print (" -> ") ;
+      lcd.print (float (entree0) * 0.0206452) ;
+      lcd.print ("V") ;
+    }
+    const int16_t entree1 = lireEntreeAnalogiqueBipolaire (ENTREE_ANALOGIQUE_BIPOLAIRE::EB1) ;
+    if (gEntreeAnalogique1 != entree1) {
+      gEntreeAnalogique1 = entree1 ;
+      lcd.setCursor (4, 3) ;
+      lcd.print ("               ") ;
+      lcd.setCursor (4, 3) ;
+      lcd.print (entree1) ;
+      lcd.print (" -> ") ;
+      lcd.print (float (entree1) * 0.0206452) ;
+      lcd.print ("V") ;
+    }
+  }
+//--- Quatrième action : action des poussoirs sur les sorties logiques
+  actionSortieLogique (SORTIE_LOGIQUE::S0, etatPoussoir (POUSSOIR::P0)) ;
+  actionSortieLogique (SORTIE_LOGIQUE::S1, etatPoussoir (POUSSOIR::P1)) ;
+  actionSortieLogique (SORTIE_LOGIQUE::S2, etatPoussoir (POUSSOIR::P2)) ;
+  actionSortieLogique (SORTIE_LOGIQUE::S3, etatPoussoir (POUSSOIR::P3)) ;
+}
+
+//--------------------------------------------------------
---
+++ carte-nucleo32-asservissement/croquis-exemples-carte-prototype/carte-asservissement.h
@@ -0,0 +1,116 @@
+//-------------------------------------------------------------------------------------------------
+// CONFIGURATION DE LA CARTE ASSERVISSEMENT
+//-------------------------------------------------------------------------------------------------
+
+#pragma once
+
+#include <LiquidCrystal.h>
+
+//-------------------------------------------------------------------------------------------------
+// Configuration : cette fonction  est à appeler une et une seule fois, dans le setup.
+//-------------------------------------------------------------------------------------------------
+
+void configurerCarteAsservissement () ;
+
+//-------------------------------------------------------------------------------------------------
+// Afficheur LCD
+// Les fonctions appelables sont décrites à l'URL :
+//     https://www.arduino.cc/en/Reference/LiquidCrystal
+//-------------------------------------------------------------------------------------------------
+
+extern LiquidCrystal lcd ;
+
+//-------------------------------------------------------------------------------------------------
+// LEDS
+//-------------------------------------------------------------------------------------------------
+
+enum class LED : uint8_t {
+  L0 = 1 << 0,
+  L1 = 1 << 1,
+  L2 = 1 << 2,
+  L3 = 1 << 3,
+  L4 = 1 << 4
+} ;
+
+//--- inValue à true -> led allumée, inValue à false -> led éteinte
+void actionLed (const LED inLed, const  bool inValue) ;
+
+//-------------------------------------------------------------------------------------------------
+// POUSSOIRS
+//-------------------------------------------------------------------------------------------------
+
+enum class POUSSOIR : uint8_t {
+  P0 = 1 << 0,
+  P1 = 1 << 1,
+  P2 = 1 << 2,
+  P3 = 1 << 3,
+  P4 = 1 << 4
+} ;
+
+//--- Poussoir relâché -> retourne false, appuyé -> retourne true
+bool etatPoussoir (const POUSSOIR inPoussoir) ;
+
+//-------------------------------------------------------------------------------------------------
+// ENCODEUR NUMÉRIQUE
+//-------------------------------------------------------------------------------------------------
+
+//--- Clic relâché -> retourne false, appuyé -> retourne true
+bool etatClic (void) ; 
+
+void fixerGammeEncodeur (const int32_t inBorneInf, const int32_t inBorneSup) ;
+
+int32_t valeurEncodeur (void) ;
+
+//-------------------------------------------------------------------------------------------------
+// SORTIES LOGIQUES
+//-------------------------------------------------------------------------------------------------
+
+enum class SORTIE_LOGIQUE : uint8_t {S0, S1, S2, S3} ;
+
+void actionSortieLogique (const SORTIE_LOGIQUE inSortieLogique, const bool inValue) ;
+
+//-------------------------------------------------------------------------------------------------
+// SORTIES ANALOGIQUES UNIPOLAIRES
+//-------------------------------------------------------------------------------------------------
+
+enum class SORTIE_ANALOGIQUE_UNIPOLAIRE {
+  SU0, // Sortie analogique unipolaire 0
+  SU1  // Sortie analogique unipolaire 1
+} ;
+
+void actionSortieAnalogiqueUnipolaire (const SORTIE_ANALOGIQUE_UNIPOLAIRE inSortieAnalogique, const uint8_t inValue) ;
+
+//-------------------------------------------------------------------------------------------------
+// SORTIES ANALOGIQUES BIPOLAIRES
+//-------------------------------------------------------------------------------------------------
+
+enum class SORTIE_ANALOGIQUE_BIPOLAIRE {
+  SB0, // Sortie analogique unipolaire 0
+  SB1  // Sortie analogique unipolaire 1
+} ;
+
+void actionSortieAnalogiqueBipolaire (const SORTIE_ANALOGIQUE_BIPOLAIRE inSortieAnalogique, const int8_t inValue) ;
+
+//-------------------------------------------------------------------------------------------------
+// ENTRÉES ANALOGIQUES UNIPOLAIRES
+//-------------------------------------------------------------------------------------------------
+
+enum class ENTREE_ANALOGIQUE_UNIPOLAIRE {
+  EU0, // Entrée analogique 0
+  EU1  // Entrée analogique 1
+} ;
+
+uint16_t lireEntreeAnalogiqueUnipolaire (const ENTREE_ANALOGIQUE_UNIPOLAIRE inEntreeAnalogique) ;
+
+//-------------------------------------------------------------------------------------------------
+// ENTRÉES ANALOGIQUES BIPOLAIRES
+//-------------------------------------------------------------------------------------------------
+
+enum class ENTREE_ANALOGIQUE_BIPOLAIRE {
+  EB0, // Entrée analogique 0
+  EB1  // Entrée analogique 1
+} ;
+
+int16_t lireEntreeAnalogiqueBipolaire (const ENTREE_ANALOGIQUE_BIPOLAIRE inEntreeAnalogique) ;
+
+//-------------------------------------------------------------------------------------------------
